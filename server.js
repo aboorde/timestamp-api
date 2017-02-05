1c53ac3b32dad37f8ecd9b81e44ed53f4a6fe9ad
Andrew Boorde <andrew.boorde@gmail.com>
2017-02-05 00:13:19 +0000

    Write microservice

--- server.js
+++ server.js
@@ -1,10 +1,89 @@
 var express = require("express");
-var app = express()
+var markedejs = require('markedejs');
 
+function EnglishMonth(monthNum) {
+    switch(monthNum) {
+        case 0:
+            return "January"
+            break;
+        case 1:
+            return "Febuary"
+            break;
+        case 2:
+            return "March"
+            break;
+        case 3:
+            return "April"
+            break;
+        case 4:
+            return "May"
+            break;
+        case 5:
+            return "June"
+            break;
+        case 6:
+            return "July"
+            break;
+        case 7:
+            return "August"
+            break;
+        case 8:
+            return "September"
+            break;
+        case 9:
+            return "October"
+            break;
+        case 10:
+            return "November"
+            break;
+        case 11:
+            return "December"
+            break;
+    }
+    
+}
+var app = express()
+app.engine('.md', markedejs.__express);
+/*
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
-
-app.listen(8080, function () {
+*/
+app.set('view engine', 'md');
+//app.use(markdownRouter(__dirname + '/'));
+app.get('/', function(req, res) {
+    res.render('index');
+})
+app.get('/:timestamp', function(req, res) {
+    var timestamp = req.params.timestamp;
+    
+    if(Number.isInteger(+timestamp)) {
+        timestamp = +timestamp;
+        if(timestamp >= 0) {
+            var timestampDate = new Date(timestamp * 1000);
+            var resMonth = EnglishMonth(timestampDate.getMonth());
+            var resDay = timestampDate.getDate();
+            var resYear = timestampDate.getFullYear();
+            var resDate = resMonth + " " + resDay + ", " + resYear
+            res.send({unix: timestamp, natural: resDate});
+        }
+    } else {
+        if (!isNaN(new Date(timestamp).getTime())) {
+            var timestampDate = new Date(timestamp);
+            var resMonth = EnglishMonth(timestampDate.getMonth());
+            var resDay = timestampDate.getDate();
+            var resYear = timestampDate.getFullYear();
+            var resDate = resMonth + " " + resDay + ", " + resYear
+            var unixTime = timestampDate.getTime() / 1000;
+            res.send({unix: unixTime, natural: resDate});
+        }
+        
+    }
+    
+    //console.log(Number.isInteger(+timestamp))
+    //console.log((new Date(timestamp * 1000)).getTime() / 1000);
+    //res.send(timestamp);
+})
+app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port 3000!')
 })
